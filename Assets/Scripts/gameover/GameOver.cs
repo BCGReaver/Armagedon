e93db7364a4ccfa6f03378d24fc23de5455fe751
Jairo-Rodriguez-123 <jrodriguez40565@ucq.edu.mx>
2024-11-29 14:18:54 -0600

    game over
    se agrego la pantalla de game over

---
+++ Assets/Scripts/gameover/GameOver.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class GameOver : MonoBehaviour
+{
+
+    public TMP_Text textPuntos;
+
+    public GameObject gameOverPanel;
+
+
+    public void MostarGameOver()
+    {
+        gameOverPanel.SetActive(true);
+
+    }
+
+
+
+
+
+
+
+
+}
